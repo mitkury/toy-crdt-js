07535e589f6cff1d7d65851441628ef704549b13
Dmitry Kury <dima.kurilchenko@gmail.com>
2023-02-25 00:46:24 +0300

    Change the visual of the sync button. Turn it into a toggle

--- js/board/boardDemo.js
+++ js/board/boardDemo.js
@@ -90,7 +90,7 @@ class BoardView extends EventTarget {
 
 
             canvasEl.addEventListener('contextmenu', event => {
-                event.preventDefault()
+                //event.preventDefault()
             })
         })
 
@@ -676,14 +676,32 @@ class SyncRealtimeButton {
         this.#containerEl = div(parentElement, wrapperEl => {
             wrapperEl.classList.add('sync-button-wrapper')
 
-            div(wrapperEl, buttonEl => {
-                buttonEl.classList.add('button')
-                buttonEl.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6"><path fill-rule="evenodd" d="M4.755 10.059a7.5 7.5 0 0112.548-3.364l1.903 1.903h-3.183a.75.75 0 100 1.5h4.992a.75.75 0 00.75-.75V4.356a.75.75 0 00-1.5 0v3.18l-1.9-1.9A9 9 0 003.306 9.67a.75.75 0 101.45.388zm15.408 3.352a.75.75 0 00-.919.53 7.5 7.5 0 01-12.548 3.364l-1.902-1.903h3.183a.75.75 0 000-1.5H2.984a.75.75 0 00-.75.75v4.992a.75.75 0 001.5 0v-3.18l1.9 1.9a9 9 0 0015.059-4.035.75.75 0 00-.53-.918z" clip-rule="evenodd" /></svg> Sync`
-                buttonEl.addEventListener('click', e => {
-                    if (this.#on) {
-                        this.#disableSync()
-                    } else {
+            div(wrapperEl, toggleEl => {
+                toggleEl.classList.add('toggle')
+
+                const span = element('span', toggleEl)
+                span.innerText = 'Sync'
+
+                const switchEl = element('label', toggleEl)
+                switchEl.classList.add('switch')
+                const checkboxEl = element('input', switchEl)
+                checkboxEl.setAttribute('type', 'checkbox')
+                const bgEl = div(switchEl)
+                bgEl.classList.add('bg')
+                const knobEl = div(switchEl)
+                knobEl.classList.add('knob')
+                const labelOffEl = div(switchEl)
+                labelOffEl.classList.add('label-off')
+                labelOffEl.innerText = 'OFF'
+                const labelOnEl = div(switchEl)
+                labelOnEl.classList.add('label-on')
+                labelOnEl.innerText = 'ON'
+                
+                checkboxEl.addEventListener('change', e => {
+                    if (checkboxEl.checked) {
                         this.#enableSync()
+                    } else {
+                        this.#disableSync()
                     }
                 })
             })
